20c64dc46a2fddf6f571b0f45836327b762c725f
KEINOS <github@keinos.com>
2018-03-10 20:33:52 +0900

    feat: Initial commit that works

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Practice_PHP-MeCab
-Practice repo to use `php-mecab.so` PHP extension module in macOS HighSierra (OSX 10.13.3)
+# PHP-MeCab を使うサンプル
+
+PHP の MeCab モジュールが使える場合はモジュール版 PHP-MeCab を利用し、モジュールが使えない場合で、コマンド・ラインから `$ mecab` が利用できる場合は、`exec()`で代用します。
+
+## 使い方
+
+1. `set_dictionary()` の引数に辞書ファイルのパスを指定する。
+1. `parse_mecab()` の引数に分かち処理をしたい**文字列**を指定する。
+
+## サンプルの使い方
+
+1. PHP ファイルに `index.php` のソースをコピペします。
+1. 使用する辞書ファイルのパスを `$path_dic_mecab` の値に設定し保存します。
+1. ファイルに実行パーミッション（'0755'など）があることを確認します。
+1. コマンド・ラインから `$ php index.php` を実行します。
+
+サンプル文書を MeCab で分かち処理をした配列の結果が出力されます。
+
+## 動作検証済み環境
+
+|確認|内容|
+|:---|:---|
+|検証日|2018/03/10|
+|OS|macOS High Sierra（OSX 10.13.3）|
+|マシン| MacBookPro（Retina, 13-inch, Early 2015）|
+|`$ php -v`|PHP 7.1.8 (cli) (built: Aug  7 2017 15:02:45) ( NTS )<br>Copyright (c) 1997-2017 The PHP Group<br>Zend Engine v3.1.0, Copyright (c) 1998-2017 Zend Technologies|
+|`$ mecab -v`|mecab of 0.996|
+|`$ php -r "phpinfo();" | grep MeCab`|MeCab Support => enabled<br>MeCab Library => 0.996 => 0.996|
+
---
+++ index.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * php-mecab/examples.
+ * parse string, wakati output format
+ * charset=utf-8
+ */
+
+/* Constants -------------------------------------------------------- */
+
+const USE_MECAB_OS  = false; //CLI経由でMeCabを利用
+const USE_MECAB_DL  = true;  //モジュール(DynamicLibrary)版MeCabを利用
+const UNAVAILABLE   = null;
+const FILE_NOT_FOND = '404 File Not Found';
+const SIZE_BUFFER   = 8192 * 2;
+
+/* Sample Data ------------------------------------------------------ */
+
+$string = <<<EOS
+MeCab は 京都大学情報学研究科−日本電信電話株式会社コミュニケーション科学基礎研究所
+共同研究ユニットプロジェクトの一環として開発されたオープンソース形態素解析エンジンです。
+言語、辞書、コーパスに依存しない汎用的な設計を基本方針としています。
+EOS;
+
+/* Main Program ----------------------------------------------------- */
+
+//$path_dic_mecab = '/usr/local/lib/mecab/dic/ipadic/sys.dic';
+//$path_dic_mecab = '/usr/local/lib/mecab/dic/mecab-ipadic-neologd/sys.dic';
+$path_dic_mecab = '/PATH/TO/YOUR/DICTIONARY/FILE.dic';
+
+set_dictionary($path_dic_mecab);
+
+$result = parse_mecab($string);
+print_r($result);
+
+die;
+
+/* ------------------------------------------------------------------ */
+
+function get_path_dic_mecab()
+{
+    return (defined('PATH_FILE_DIC')) ? PATH_FILE_DIC : UNAVAILABLE;
+}
+
+function get_mode_mecab()
+{
+    return (defined('USE_MECAB_MODE')) ? USE_MECAB_MODE : UNAVAILABLE;
+}
+
+function is_mecab_available()
+{
+    $result = extension_loaded('mecab');
+
+    if ($result) {
+        set_mode_mecab(USE_MECAB_DL);
+        return $result;
+    }
+
+    if (! $result) {
+        $_module_suffix = (PHP_SHLIB_SUFFIX == 'dylib') ? 'so' : PHP_SHLIB_SUFFIX;
+        if (dl('mecab.' . $_module_suffix)) {
+            $result = true;
+            set_mode_mecab(USE_MECAB_DL);
+        }
+    }
+
+    if (! $result) {
+        $cmd = "mecab -v";
+        $msg = `$cmd`;
+        if (strpos(strtolower($msg), 'not found') !== false) {
+            $result = true;
+            set_mode_mecab(USE_MECAB_OS);
+        }
+    }
+
+    return $result;
+}
+
+
+function parse_mecab(string $string)
+{
+    if (! is_mecab_available()) {
+        die('Error: No MeCab found. Please install MeCab.' . PHP_EOL);
+    }
+
+    switch (get_path_dic_mecab()) {
+        case UNAVAILABLE:
+            die(
+                'Error: No dictionary set. ' .
+                'Use \'set_dictionary()\' before use.' .
+                PHP_EOL
+            );
+            break;
+        case FILE_NOT_FOND:
+            die(
+                'Error: Dictinonary not found. ' .
+                'Set the proper MeCab dictionary.' .
+                PHP_EOL
+            );
+            break;
+        default:
+            //
+            break;
+    }
+
+    if (USE_MECAB_DL == get_mode_mecab()) {
+        $parsed = parse_mecab_dl($string);
+    }
+    if (USE_MECAB_OS == get_mode_mecab()) {
+        $parsed = parse_mecab_os($string);
+    }
+
+    $lines  = explode(PHP_EOL, $parsed);
+    $result = array();
+
+    foreach ($lines as $key => $line) {
+        $tmp   = explode("\t", $line);
+
+        if (! isset($tmp[1])) {
+            continue;
+        }
+        $word  = trim($tmp[0]);
+        $mecab = explode(',', $tmp[1]);
+        $result[$key] = [
+            'word'     => $word,
+            'parsed'   => $mecab,
+        ];
+    }
+
+    return $result;
+}
+
+function parse_mecab_dl(string $string)
+{
+    $path_file_dic = get_path_dic_mecab();
+
+    $mecab = new MeCab\Tagger([
+        '-d' => $path_file_dic,
+    ]);
+
+    return $mecab->parseToString($string);
+}
+
+function parse_mecab_os(string $string)
+{
+    //UTF-8 Unicode text, with no line terminators エラー回避用に改行追加
+    $string = $string . PHP_EOL;
+
+    $name_file_temp = 'mecab_' . md5(microtime());
+    $path_file_temp = temporaryFile($name_file_temp, $string);
+    $path_file_dic  = get_path_dic_mecab();
+    $size_buffer    = SIZE_BUFFER;
+
+    $cmd = "mecab -d \"${path_file_dic}\" \"${path_file_temp}\" -b ${size_buffer}";
+
+    return `$cmd`;
+}
+
+
+function set_dictionary($path)
+{
+    if (! file_exists($path)) {
+        $path = FILE_NOT_FOND;
+    }
+
+    if (! defined('PATH_FILE_DIC')) {
+        define('PATH_FILE_DIC', $path);
+    }
+
+    return PATH_FILE_DIC;
+}
+
+function set_mode_mecab(bool $mecab_mode)
+{
+    if (! defined('USE_MECAB_MODE')) {
+        define('USE_MECAB_MODE', $mecab_mode);
+    }
+
+    return USE_MECAB_MODE;
+}
+
+/**
+ * @ref http://php.net/manual/ja/function.tmpfile.php#120062
+ */
+function temporaryFile($name, $content)
+{
+    $file = DIRECTORY_SEPARATOR .
+            trim(sys_get_temp_dir(), DIRECTORY_SEPARATOR) .
+            DIRECTORY_SEPARATOR .
+            ltrim($name, DIRECTORY_SEPARATOR);
+
+    file_put_contents($file, $content);
+
+    register_shutdown_function(function () use ($file) {
+        unlink($file);
+    });
+
+    return $file;
+}
